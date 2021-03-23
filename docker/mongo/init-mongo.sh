fbf6bbfdbdd858d8cb6a70627a6b5110ad8b41ad
iryabuhin <55144114+iryabuhin@users.noreply.github.com>
2021-03-23 23:07:53 +0300

    fix mongo db user not being created withcredentials specified in dotenv

--- docker/mongo/init-mongo.sh
+++ docker/mongo/init-mongo.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-set -eu
-mongo -- "$MONGO_DB" <<EOF
+set -u
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+
     var admin = db.getSiblingDB('admin');
+
     admin.auth(rootUser, rootPassword);
 
     var user = '$MONGO_USERNAME';
-    var passwd = '${MONGO_PASSWORD-}' || user;
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
-EOF
+    var passwd = '$MONGO_PASSWORD' || user;
 
-if [ $? -eq 0 ]; then
-    echo "User $MONGO_USERNAME with password $MONGO_PASSWORD added to $MONGO_DB"
-else
-    echo '[ERROR] Failed adding user to database, aborting..' 1>&2
-fi
+    var app = db.getSiblingDB('app');
+
+    app.createUser({
+        user: user,
+        pwd: passwd,
+        roles: [
+            {role: 'readWrite', db: '$MONGO_DB'}
+        ]
+    });
+EOF
