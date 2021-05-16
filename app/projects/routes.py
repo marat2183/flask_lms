f3ca1834847fbef65741b1eb36c10884c5fb3c72
Ivan Ryabukhin <ryabuchin@gmail.com>
2021-05-16 13:48:17 +0300

    fix mongoengine queries and project API routes

--- app/projects/routes.py
+++ app/projects/routes.py
@@ -7,6 +7,7 @@ from pymongo import ReturnDocument
 from app.utils import success, error
 from .sheets import sheets_team_add_member
 from app.models import Project, User
+from app.utils import required_json_arguments
 from mongoengine import DoesNotExist
 from mongoengine import signals
 import json
@@ -20,58 +21,52 @@ def index():
     return render_template('projects/index.html', projects=projects, user=current_user)
 
 
-@projects.route('/api/projects/<id>/', methods=['GET'])
+@projects.route('/api/<id>/', methods=['GET'])
 @login_required
 def projects_get(id: str):
     try:
-        doc = Project.objects.get(id=id)
+        doc = Project.objects(id=id).first()
         return success('found', data=json.loads(doc.to_json()))
     except DoesNotExist:
         return error(f'Project with ID {id} does not exist', status_code=404)
 
 
-
-@projects.route('/api/projects/find/<query>', methods=['GET'])
+@projects.route('/api/find/<query>', methods=['GET'])
 @login_required
-def get_by_name_proj(query: str):
+def get_project_by_name(query: str):
     try:
         doc = Project.objects.get(name__icontains=query)
         return success('found', data=json.loads(doc.to_json()))
     except DoesNotExist:
         return error('Project with such name does not exist', status_code=404)
 
-
-
-@projects.route('/api/projects/<id>/available', methods=['GET'])
+@projects.route('/api/available', methods=['GET'])
 @login_required
-def check_project_availability(id: str):
+def check_project_availability():
     try:
-        docs = Project.objects.get(available=True)
+        docs = Project.objects.filter(disabledForJoin=False)
         return success('available', data=[json.loads(d.to_json()) for d in docs])
     except DoesNotExist:
         return error('No project with such ID', status_code=404)
 
-@projects.route('/api/projects/<id>/teams', methods=['GET'])
+@projects.route('/api/<id>/join', methods=['POST'])
+@required_json_arguments
 @login_required
-def get_project_teams(id: str):
-    try:
-        doc = Project.objects.get(id=id)
-        return success('teams for project', data=json.loads(doc.teams.to_json()))
-    except DoesNotExist:
-        return error(f'No project with ID {id}', status_code=404)
-
-
-@projects.route('/api/projects/<id>/join', methods=['POST', 'PUT'])
-@login_required
-def join_project(id: str, name: str, group: str, team: str):
-    if len(Project.objects(teams__members__name=name)) >= 1:
+def join_project(id: str, team_number: str):
+    if len(Project.objects.filter(teams__members__in=[current_user])) >= 1:
         return error('Вы уже записаны на проект')
 
     if current_user.role != 'Студент':
-        return error('На проект могут быть записаны только студенты!', status_code=409)
+        return error('На проект могут записаться только студенты!', status_code=409)
 
     try:
-        Project.objects.find(id=id).update_one(push__teams__S__members=current_user)
-        return success(f'Вы успешно записаны ны проект', data={})
+        # TODO fixme, probably an incorrect insert query
+        rv = Project.objects.get(id=id).update(
+            **{f'push__teams__{team_number}__members': current_user}
+        )
+        if rv:
+            return success(f'Вы успешно записаны на проект', data={})
+        else:
+            return error('Update failed due to an unknown database error', status_code=500)
     except DoesNotExist:
         return error(f'No project with ID {id}', status_code=404)
